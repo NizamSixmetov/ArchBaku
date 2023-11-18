b3e550bfa77b25d193c19c17acd848b7f588e9b0
Shikhmetov Nizam <134426138+NizamSixmetov@users.noreply.github.com>
2023-11-19 01:24:41 +0400

    ArchBaku

--- src/Components/AboutUs/index.jsx
+++ src/Components/AboutUs/index.jsx
@@ -5,12 +5,12 @@ import Button from '../ButtonUniversal/Button';
 import HomeAboutComponent from '../HomeAboutComponent';
 
 const AboutUs = () => {
-    return (
-        <>
-            <img className={`${styles.imagepos}`} src="./AboutPosition.svg" alt="" />
-            <div className="container">
-                <div className={`${styles.containerChild}`}>
-                    {/* <div className={`${styles.leftImage}`}>
+  return (
+    <>
+      <img className={`${styles.imagepos}`} src="./AboutPosition.svg" alt="" />
+      <div className="container">
+        <div className={`${styles.containerChild}`}>
+          {/* <div className={`${styles.leftImage}`}>
                         <div className={`${styles.positionDivUp}`}></div>
                         <div className={`${styles.positionDivDown}`}></div>
                         <img src="./AboutUsImage.svg" alt="" />
@@ -38,27 +38,27 @@ const AboutUs = () => {
                         </p>
                         <Button buttonStyle={'brown'} text={'Ətraflı'} link={'/about'} icon={<BiChevronRight />} />
                     </div> */}
-                    <div className={`${styles.displayFlex}`}>
-                        <div>
-                            <HomeAboutComponent className={`${styles.flexDiv}`}
-                                AboutUsStyles={'homeAboutUs'}
-                                imgElement={'../../../public/AboutUsImage.svg'}
-                                topFrame={''}
-                                bottomFrame={''}
-                                h2Element={'HAQQIMIZDA'}
-                                h2ElementLine={''}
-                                firstP={'Archbaku MMC 25 sentyabr 2019 cu ildən təcrübəli mütəxəssis heyəti ilə xidmət göstərən tikinti şirkətidir. İqtisadiyyatın ən önəmli sahələrindən biri olan infrastruktur sektorunda daim yeniliklərə, inkişafa önəm vermişik.'}
-                                twoP={'Bizim ən önəmli missiyamız yüksək keyfiyyətli tikinti və mühəndislik təcrübəmizdən istifadə edərək ən son texnikalarla müştərilərimizin etimadını qazanmaqdır.'} />
+          <div className={`${styles.displayFlex}`}>
+            <div>
+              <HomeAboutComponent className={`${styles.flexDiv}`}
+                AboutUsStyles={'homeAboutUs'}
+                imgElement={'../../../public/AboutUsImage.svg'}
+                topFrame={''}
+                bottomFrame={''}
+                h2Element={'HAQQIMIZDA'}
+                h2ElementLine={''}
+                firstP={'Archbaku MMC 25 sentyabr 2019 cu ildən təcrübəli mütəxəssis heyəti ilə xidmət göstərən tikinti şirkətidir. İqtisadiyyatın ən önəmli sahələrindən biri olan infrastruktur sektorunda daim yeniliklərə, inkişafa önəm vermişik.'}
+                twoP={'Bizim ən önəmli missiyamız yüksək keyfiyyətli tikinti və mühəndislik təcrübəmizdən istifadə edərək ən son texnikalarla müştərilərimizin etimadını qazanmaqdır.'} />
 
-                        </div>
-                        <div>
-                            <Button buttonStyle={'brown'} text={'Ətraflı'} link={'/about'} icon={<BiChevronRight />} />
-                        </div>
-                    </div>
-                </div>
-            </div >
-        </>
-    )
+            </div>
+            <div>
+              <Button buttonStyle={'brown'} text={'Ətraflı'} link={'/about'} icon={<BiChevronRight />} />
+            </div>
+          </div>
+        </div>
+      </div >
+    </>
+  )
 }
 
 export default AboutUs
--- src/Components/ButtonUniversal/Button.jsx
+++ src/Components/ButtonUniversal/Button.jsx
@@ -3,31 +3,31 @@ import styles from './style.module.css';
 import { BiChevronRight } from 'react-icons/bi'
 
 const Button = ({ text, buttonStyle, icon, link }) => {
-    let universalClass = ''
-    switch (buttonStyle) {
-        case 'brown':
-            universalClass = styles.brownButton
-            icon = <BiChevronRight />
-        default:
-            break;
-        case 'green':
-            universalClass = styles.greenButton
-            icon = <BiChevronRight />
-            break;
-        case 'transparent':
-            universalClass = styles.transparentButton
-            icon = <BiChevronRight />
-            break;
-        case 'transparentWhite':
-            universalClass = styles.transparentButtonWhite
-            icon = <BiChevronRight />   
-            break;
-    }
-    return (
-        <Link to={link}>
-            <button className={`d-flex align-items-center ${universalClass}`}>{text}{icon}</button>
-        </Link>
-    )
+  let universalClass = ''
+  switch (buttonStyle) {
+    case 'brown':
+      universalClass = styles.brownButton
+      icon = <BiChevronRight />
+    default:
+      break;
+    case 'green':
+      universalClass = styles.greenButton
+      icon = <BiChevronRight />
+      break;
+    case 'transparent':
+      universalClass = styles.transparentButton
+      icon = <BiChevronRight />
+      break;
+    case 'transparentWhite':
+      universalClass = styles.transparentButtonWhite
+      icon = <BiChevronRight />
+      break;
+  }
+  return (
+    <Link to={link}>
+      <button className={`d-flex align-items-center ${universalClass}`}>{text}{icon}</button>
+    </Link>
+  )
 }
 
 export default Button;
